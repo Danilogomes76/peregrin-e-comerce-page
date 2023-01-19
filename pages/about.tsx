ab6b0267f5185eed9269c0c0ddc10a275eb7b725
Danilogomes76 <danilo762410@gmail.com>
2023-01-19 01:54:46 -0300

    Add animation in category

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import logo from '../src/images/logo-high-size.svg'
-import styles from '../styles/pageStyles/about.module.scss'
+import logo from '../src/images/logo-high-size.svg';
+import styles from '../styles/pageStyles/about.module.scss';
 
 const About: React.FC = () => {
   return (
@@ -12,10 +12,19 @@ const About: React.FC = () => {
       <main className={styles.container}>
         <section className={styles.apresentation}>
           <h1>About us</h1>
-          <p>The Peregrin Store is an online store that offers a wide variety of products to meet the needs of our customers. We believe in providing an exceptional shopping experience with a wide range of high quality items at competitive prices. With the Peregrin Store, you can find everything you need in one place, from electronics and clothing to home and garden products. Plus, we offer fast, secure shipping and exceptional customer service to ensure your shopping experience is the best it can be. Shop with confidence at the Peregrin Store.</p>
+          <p>
+            The Peregrin Store is an online store that offers a wide variety of
+            products to meet the needs of our customers. We believe in providing
+            an exceptional shopping experience with a wide range of high quality
+            items at competitive prices. With the Peregrin Store, you can find
+            everything you need in one place, from electronics and clothing to
+            home and garden products. Plus, we offer fast, secure shipping and
+            exceptional customer service to ensure your shopping experience is
+            the best it can be. Shop with confidence at the Peregrin Store.
+          </p>
         </section>
         <section className={styles.logo}>
-          <Image src={logo} alt="Logo"/>
+          <Image src={logo} alt="Logo" />
         </section>
       </main>
     </>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,19 +1,15 @@
 import Head from 'next/head';
 import styles from '../styles/pageStyles/profile.module.scss';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const pages: React.FC = () => {
-  const notify = () => toast('Wow so easy!');
   return (
     <>
       <Head>
         <title>Profile</title>
       </Head>
-      <div className={styles.container}>
-        <button onClick={notify}>Notify!</button>
-        <ToastContainer />
-      </div>
+      <main>
+        <h1>profile</h1>
+      </main>
     </>
   );
 };
